4e7e01dfba8f098dabdc4aa83f22a17fb595017a
jhonnymarques <jhonnymarques.r@gmail.com>
2020-08-10 17:27:26 -0300

    initial commit

---
+++ src/layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import GlobalStyle from "./globalstyle";
+import Header from "./components/Header/Header";
+
+const Layout = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
+  );
+};
+
+export default Layout;
